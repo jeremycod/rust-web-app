af0540595a6380228091813ad49f4f22ff5dde06
zoran.jeremic.-nd <zoran.jeremic.-nd@disney.com>
2025-05-26 19:19:38 -0700

    Initial version

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+#![allow(unused)] // For early development.
+
+// region:    --- Modules
+
+mod ctx;
+mod error;
+mod log;
+mod model;
+mod web;
+
+pub use self::error::{Error, Result};
+
+use crate::model::ModelManager;
+use crate::web::mw_auth::mw_ctx_resolve;
+use crate::web::mw_res_map::mw_reponse_map;
+use crate::web::{routes_login, routes_static};
+use axum::{middleware, Router};
+use tokio::net::TcpListener;
+use std::net::SocketAddr;
+use tower_cookies::CookieManagerLayer;
+use std::sync::LazyLock;
+// endregion: --- Modules
+
+#[tokio::main]
+async fn main() -> Result<()> {
+	// Initialize ModelManager.
+	let mm = ModelManager::new().await?;
+	// -- Define Routes
+	// let routes_rpc = rpc::routes(mm.clone())
+	//   .route_layer(middleware::from_fn(mw_ctx_require));
+	static STATIC_PATH: &str = "./";
+	static STATIC_PATH_STRING: LazyLock<String> = LazyLock::new(|| "./".to_string());
+
+	let routes_all = Router::new()
+		.merge(routes_login::routes())
+		// .nest("/api", routes_rpc)
+		.layer(middleware::map_response(mw_reponse_map))
+		.layer(middleware::from_fn_with_state(mm.clone(), mw_ctx_resolve))
+		.layer(CookieManagerLayer::new())
+		.fallback_service(routes_static::serve_dir(&STATIC_PATH_STRING));
+
+	// region:    --- Start Server
+	let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+	println!("{:<12} - {:?}\n", "LISTENING", listener.local_addr());
+	axum::serve(listener, routes_all.into_make_service())
+		.await
+		.unwrap();
+	// endregion: --- Start Server
+
+	Ok(())
+}
---
+++ src/web/routes_login.rs
@@ -0,0 +1,33 @@
+use crate::web::{self, Error, Result};
+use serde::Deserialize;
+use axum::{Json, Router};
+use axum::routing::post;
+use serde_json::{Value, json};
+use tower_cookies::{Cookie, Cookies};
+
+
+pub fn routes() -> Router {
+	Router::new().route("/api/login", post(api_login))
+}
+async fn api_login(cookies: Cookies, payload: Json<LoginPayload>) -> Result<Json<Value>>{
+	println!("->> {:<12} - api_login", "HANDLER");
+	if payload.username != "demo1" || payload.pwd != "welcome" {
+		return Err(Error::LoginFail);
+	}
+	//TODO: Implement real authentication
+	cookies.add(Cookie::new(web::AUTH_TOKEN, "user-1.exp.sign"));
+
+	// Create the success body
+	let body = Json(json!({
+        "result": {
+            "success": true
+        }
+    }));
+	Ok(body)
+}
+
+#[derive(Debug, Deserialize)]
+struct LoginPayload {
+	username: String,
+	pwd: String
+}
