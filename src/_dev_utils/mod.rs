56d4b47bdc26206dff4b21186d4daffb86429984
zoran.jeremic.-nd <zoran.jeremic.-nd@disney.com>
2025-05-30 21:30:27 -0700

    init dev db

---
+++ src/_dev_utils/mod.rs
@@ -0,0 +1,16 @@
+// region: --- Modules
+
+mod dev_db;
+
+use log::info;
+use tokio::sync::OnceCell;
+
+pub async fn init_dev(){
+    static INIT: OnceCell<()> = OnceCell::const_new();
+
+    INIT.get_or_init(|| async {
+        info!("{:<12} - init_dev_all()", "FOR-DEV-ONLY");
+        dev_db::init_dev_db().await.unwrap();
+    })
+        .await;
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod error;
 mod log;
 mod model;
 mod web;
+pub mod _dev_utils;
 
 pub use self::error::{Error, Result};
 pub use config::config;
@@ -32,6 +33,10 @@ async fn main() -> Result<()> {
 		.with_target(false)
 		.with_env_filter(EnvFilter::from_default_env())
 		.init();
+	
+	// -- FOR DEV ONLY
+	_dev_utils::init_dev().await;
+	
 	// Initialize ModelManager.
 	let mm = ModelManager::new().await?;
 	// -- Define Routes
