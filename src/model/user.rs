36501e55569e2badb4768456bb83626e8a3e5baf
zoran.jeremic.-nd <zoran.jeremic.-nd@disney.com>
2025-06-08 19:22:56 -0700

    user model basic setup

--- src/model/user.rs
+++ src/model/user.rs
@@ -10,40 +10,39 @@ use uuid::Uuid;
 
 #[derive(Clone, Fields, FromRow, Debug, Serialize)]
 pub struct User {
-    pub id: i64,
-    pub username: String,
+	pub id: i64,
+	pub username: String,
 }
 
 #[derive(Deserialize)]
 pub struct UserForCreate {
-    pub username: String,
-    pub pwd_clear: String,
+	pub username: String,
+	pub pwd_clear: String,
 }
-
 
 #[derive(Fields)]
 struct UserForInsert {
-    pub username: String,
+	pub username: String,
 }
 
 #[derive(Clone, FromRow, Fields, Debug)]
 pub struct UserForLogin {
-    pub id: i64,
-    pub username: String,
+	pub id: i64,
+	pub username: String,
 
-    // -- pwd and token info
-    pub pwd: Option<String>, // encrypted, #_scheme_id_#...
-    pub pwd_salt: Uuid,
-    pub token_salt: Uuid,
+	// -- pwd and token info
+	pub pwd: Option<String>, // encrypted, #_scheme_id_#...
+	pub pwd_salt: Uuid,
+	pub token_salt: Uuid,
 }
 
 #[derive(Clone, FromRow, Fields, Debug)]
 pub struct UserForAuth {
-    pub id: i64,
-    pub username: String,
+	pub id: i64,
+	pub username: String,
 
-    // token info
-    pub token_salt: Uuid,
+	// token info
+	pub token_salt: Uuid,
 }
 
 // Marker trait
@@ -52,3 +51,61 @@ pub trait UserBy: HasFields + for<'r> FromRow<'r, PgRow> + Unpin + Send {}
 impl UserBy for User {}
 impl UserBy for UserForLogin {}
 impl UserBy for UserForAuth {}
+
+pub struct UserBmc;
+
+impl DbBmc for UserBmc {
+	const TABLE: &'static str = "user";
+}
+
+impl UserBmc {
+	pub async fn get<E>(ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<E>
+	where
+		E: UserBy,
+	{
+		base::get::<Self, _>(ctx, mm, id).await
+	}
+
+	pub async fn first_by_username<E>(
+		ctx: &Ctx,
+		mm: &ModelManager,
+		username: &str,
+	) -> Result<Option<E>>
+	where
+		E: UserBy,
+	{
+		let db = mm.db();
+		let user = sqlb::select()
+			.table(Self::TABLE)
+			.and_where("username", "=", username)
+			.fetch_optional::<_, E>(db)
+			.await?;
+
+		Ok(user)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::_dev_utils;
+	use anyhow::{Result, Context};
+	use serial_test::serial;
+	
+	#[serial]
+	#[tokio::test]
+	async fn test_first_ok_demo1() -> Result<()>{
+		
+		let mm = _dev_utils::init_test().await;
+		let ctx = Ctx::root_ctx();
+		let fx_username = "demo1";
+		
+		let user: User = UserBmc::first_by_username(&ctx, &mm, fx_username)
+			.await?
+			.context("Should have user 'demo1")?;
+		
+		assert_eq!(user.username, fx_username);
+		
+		Ok(())
+	}
+}
